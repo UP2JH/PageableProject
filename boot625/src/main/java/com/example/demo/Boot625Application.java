8074bcb866c2fc73eb7c0d1b0512c4753bcc76b4
up2jh <siwan9292@gmail.com>
2019-06-25 22:08:46 +0900

    test commit

---
+++ boot625/src/main/java/com/example/demo/Boot625Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Boot625Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Boot625Application.class, args);
+	}
+
+}
